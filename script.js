a3a7b1c0a5725a10fda6acfb87aa2ada8ac4cb65
SylviaLe <sylviale19112001@gmail.com>
2021-05-13 20:28:06 +0700

    add scrolling affect and fix the divider in gallery

---
+++ script.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+sr.reveal('.reveal-img',{delay: 200}); 
+sr.reveal('.reveal-txt',{delay: 400});
+
+sr.reveal('.about',{delay: 150});
+sr.reveal('.skills',{delay: 150});
+sr.reveal('.exp',{delay: 150});
+
+
+
